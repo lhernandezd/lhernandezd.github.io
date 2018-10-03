88f2c6227be2470adb3f47696e4512644c1252d6
Luis Hernández <ldhdv95@gmail.com>
2018-10-03 18:20:08 -0500

    Create SocialLinks component: SocialLinks.js and SocialLinks.css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import SocialLinks from './components/SocialLinks';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
+        <SocialLinks />
       </div>
     );
   }
---
+++ src/components/SocialLinks.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../styles/SocialLinks.css';
+
+const SocialLinks = (props) => {
+  return (
+    <div className="section__social">
+      <a className="social__link" target="_blank" rel="noopener noreferrer" href="https://github.com/lhernandezd">
+        <i className="fab fa-github"></i>
+      </a>
+      <a className="social__link" target="_blank" rel="noopener noreferrer" href="https://twitter.com/ldhdv1195">
+        <i className="fab fa-twitter"></i>
+      </a>
+      <a className="social__link" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/lhernandezd95/">
+        <i className="fab fa-linkedin-in"></i>
+      </a>
+      <a className="social__link" target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/luis1195/">
+        <i className="fab fa-instagram"></i>
+      </a>
+    </div>
+  )
+}
+
+export default SocialLinks;
