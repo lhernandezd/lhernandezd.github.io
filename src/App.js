dff30f88f719f89d366dd6fb863e51148e06180b
Luis Hernández <ldhdv95@gmail.com>
2018-10-04 00:45:22 -0500

    Create Projects component: Projects.js and Projects.css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import SocialLinks from './components/SocialLinks';
-import ProjectCard from './components/ProjectCard';
+import Projects from './components/Projects';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ProjectCard />
+        <Navbar />
+        <Projects />
       </div>
     );
   }
--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,15 +4,28 @@ import '../styles/ProjectCard.css';
 const ProjectCard = (props) => {
   return (
     <div className="content__card">
-      <h2 className="card__title">Portfolio</h2>
-      <div className="card__icons">
-        <i className="fab fa-react"></i>
-        <i className="fab fa-css3-alt"></i>
-      </div>
-      <span className="card__meta">React, React-router, HTML, CSS, Js</span>
-      <a className="card__link" href="https://github.com/lhernandezd/react-timer">View Project</a>
-      <a className="card__link" href="https://github.com/lhernandezd/react-timer">View Code</a>
-      <p>Single wep page application which includes my personal information and some projects.</p>
+      <h2 className="card__title">{props.name}</h2>
+      {props.icons.length < 2 ?
+        <div className="card__icons">
+          <i className={props.icons[0]}></i> 
+        </div>
+        : 
+        <div className="card__icons">
+          {props.icons.map((icon,index) => 
+            <i key={index} className={icon}></i>
+          )}
+        </div>
+      }
+      <span className="card__meta">{props.meta}</span>
+      {props.links.length < 2 ?
+        <a className="card__link" href={props.links[0]}>View Code</a>
+        : 
+        <div>
+          <a className="card__link" href={props.links[0]}>View Project</a>
+          <a className="card__link" href={props.links[1]}>View Code</a>
+        </div>
+      }
+      <p>{props.description}</p>
     </div>
   )
 }
