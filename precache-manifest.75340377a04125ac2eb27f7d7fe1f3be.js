5d494ae757958cde17a4d99f6aef10a1277b65f4
Luis Hernández <ldhdv95@gmail.com>
2018-10-06 13:42:44 -0500

    Updates

---
+++ precache-manifest.75340377a04125ac2eb27f7d7fe1f3be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d7996dd34e58a6f3b7b6311972b2fb7",
+    "url": "/static/media/Scan.8d7996dd.jpeg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "69f1ab3750dfaec5bf38",
+    "url": "/static/js/main.69f1ab37.chunk.js"
+  },
+  {
+    "revision": "b592fd750be3482b49f3",
+    "url": "/static/js/1.b592fd75.chunk.js"
+  },
+  {
+    "revision": "69f1ab3750dfaec5bf38",
+    "url": "/static/css/main.bcb37e08.chunk.css"
+  },
+  {
+    "revision": "fc9c7255fd133ebb5111ec5ee1c98eae",
+    "url": "/index.html"
+  }
+];
